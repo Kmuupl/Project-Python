d1fc4a0980405e77ad4738674ae267d11a50e6d8
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-04-09 12:55:57 +0200

    try to do game class

--- src/game.py
+++ src/game.py
@@ -1,2 +1,10 @@
 class Game:
-    def start()
+    def __init__(self, player):
+        self.player = playerself.locations = [
+            0("Dungeon",  "Dark and cold...", "dungeon enemy"),
+            1("Sewers",   "It smells...",     "sewers enemy"),
+            2("Slums",    "Ruins everywhere", "slums enemy"),
+            3("Castle",   "Cold stone walls", "castle enemy"),
+            4("Throne",   "The final room",   "BOSS"), 
+        ]
+    def next_location(self):
