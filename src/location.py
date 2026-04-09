e037c1e8058570fd5e8469f5ff0e3e42689d4b32
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-04-09 12:45:50 +0200

    add class location.

---
+++ src/location.py
@@ -0,0 +1,5 @@
+class Location:
+    def __init__(self, name: str, description: str, enemy_name; str):
+        self.name = name
+        self.description = description
+        self.enemy_name = enemy_name
